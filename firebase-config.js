169eccdca709d07bf38bfc99baac775e2ce8be16
Dheeraj Thummalapenta <dt7768181@gmail.com>
2025-07-02 07:06:11 +0000

    // Import the functions you need from the SDKs you need import { initial

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,6 @@
 // firebase-config.js
 import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
+import { getFirestore } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -15,6 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const database = getDatabase(app);
+const db = getFirestore(app);
 
-export { database };
+export { db };
