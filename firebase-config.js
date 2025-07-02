0dcc02c31ec11e23bae518aa37e57cc1aa9dfdb7
Dheeraj Thummalapenta <dt7768181@gmail.com>
2025-07-02 05:53:23 +0000

    edited

---
+++ firebase-config.js
@@ -0,0 +1,22 @@
+// firebase-config.js
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  databaseURL: "YOUR_DATABASE_URL",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+  measurementId: "YOUR_MEASUREMENT_ID"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database };
